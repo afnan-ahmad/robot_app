b3e66b12b2e30019df0737c4117190624ea2f587
Afnan Ahmad <34241491+afnan-ahmad@users.noreply.github.com>
2024-06-11 13:28:28 +0400

    Updated logging level to INFO

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import sys
 from nlu import RobotNLU
 from transcribe import RobotASR
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 robot_nlu = RobotNLU()
 robot_asr = RobotASR()
